6fe9b9f047db64848e85ec7c385b609404dda080
kraisorn <22193826+tOxicV4p0r@users.noreply.github.com>
2023-06-19 21:35:14 +0700

    week1 bookAPI

--- LittlelemonAPI/views.py
+++ LittlelemonAPI/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from django.db import IntegrityError
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.forms.models import model_to_dict
+from .models import Book
 
 # Create your views here.
+@csrf_exempt
+def books(req):
+    if(req.method == 'GET'):
+        books = Book.objects.all().values()
+        return JsonResponse({"books":list(books)})
+    elif req.method == 'POST':
+        title = req.POST.get('title')
+        author = req.POST.get('author')
+        price = req.POST.get('price')
+        book = Book(
+            title = title,
+            author = author,
+            price = price
+        )
+
+        try:
+            book.save()
+        except IntegrityError:
+            return JsonResponse({'error':'true','message':'required field missing'},status = 400)
+        return JsonResponse(model_to_dict(book),status = 201)
